114c88b711fdb3461bf54ef2ff862618371abb87
Amit Patel <apatel6152@conestogac.on.ca>
2023-01-11 13:26:54 -0500

    some css changes in skill section

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -20,7 +20,7 @@ const Skill = ({ src, profieciency }) => {
         src={src}
         alt=""
       />
-      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-[90px] h-[90px] md:w-32 md:h-32 xl:w-32 xl:h-32 rounded-full z-0">
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-[80px] h-[80px] md:w-32 md:h-32 xl:w-32 xl:h-32 rounded-full z-0">
         <div className="flex items-center justify-center h-full">
           <p className="text-xl font-bold text-black opacity-100">{profieciency}</p>
         </div>
